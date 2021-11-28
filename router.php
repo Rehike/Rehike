d9769f810c04de2a7912b9e88a01afd2fc4129a1
Nightlinbit <dcoop2004@gmail.com>
2021-11-28 14:51:20 -0700

    Preemptive shorts to watch redirect

--- router.php
+++ router.php
@@ -8,6 +8,12 @@ switch ($routerUrl->path[0]) {
     case 'watch':
         include($root . '/views/watch.php');
         break;
+    case 'shorts': // redirect to watch
+        ob_end_clean();
+        ob_start();
+        header('Location: /watch?v=' . $path[1]);
+        ob_end_flush();
+        exit();
     case 'debug':
         include($root . '/debug.php');
         break;
