3818cf0607f7141c42206b85b2392c81ef31152d
Nightlinbit <dcoop2004@gmail.com>
2022-02-16 17:09:08 -0700

    removed herobrine (fuck)

---
+++ views/channel/about.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * API Rewrite for C4 About page.
+ * 
+aboutContents: {
+    "stats": [
+        [
+            0 subscribers,
+            0 views 
+        ],
+        [Joined 31 Dec 1969]
+    ],
+    "description": [
+
+    ],
+    "details"
+}
+ */
