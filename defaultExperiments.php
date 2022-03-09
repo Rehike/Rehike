8abc614c0842e637845f191b698cca1efdb3851e
aubymori <aubyomori@gmail.com>
2022-03-08 18:28:16 -0600

    old upload button experiment

--- defaultExperiments.php
+++ defaultExperiments.php
@@ -4,7 +4,8 @@ $yt->experiment = (object) [
     'creatorMenu' => false, // true: creation menu with upload and go live; false: upload icon
     'oldRoboto' => false, // true: older version of roboto from 2016; false: normal roboto from 2017+
     'timeOnRecoms' => false, // true: show relative upload date on recommendations; false: don't show, like regular hitchhiker
-    'webV2Home' => false // true: newer uncategorized homepage, less broken; false: web v1 shelves homepage, like in regular hitchhiker
+    'webV2Home' => false, // true: newer uncategorized homepage, less broken; false: web v1 shelves homepage, like in regular hitchhiker
+    'oldUploadBtn' => true, // true: old upload button with "Upload" text; false: upload material icon; NOTE: 'creatorMenu' must be set to false for this to apply
 ];
 
 $yt->debugger = false; // does nothing at the moment, in the future this should provide tools for debugging such as viewing raw innertube responses
