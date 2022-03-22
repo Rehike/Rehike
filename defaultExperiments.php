ca57994814b735c0b4299bd67d048fa6bdc7ea7a
Coffee <99076687+YukisCoffee@users.noreply.github.com>
2022-03-22 19:54:34 +0900

    Use creation menu by default
    Temporary change as the experiment system should hopefully be reworked eventually.

--- defaultExperiments.php
+++ defaultExperiments.php
@@ -1,7 +1,7 @@
 <?php
 $yt->experiment = (object) [
     'ringo' => true, // true: new logo; false: old logo before 2017
-    'creatorMenu' => false, // true: creation menu with upload and go live; false: upload icon
+    'creatorMenu' => true, // true: creation menu with upload and go live; false: upload icon
     'oldRoboto' => false, // true: older version of roboto from 2016; false: normal roboto from 2017+
     'timeOnRecoms' => false, // true: show relative upload date on recommendations; false: don't show, like regular hitchhiker
     'webV2Home' => false, // true: newer uncategorized homepage, less broken; false: web v1 shelves homepage, like in regular hitchhiker
