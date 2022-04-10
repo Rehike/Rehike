c499adf1ad6756a2cb9cafb8f6ae992e9b1cd988
Nightlinbit <dcoop2004@gmail.com>
2022-04-10 08:09:09 -0700

    remove install method of abstract module (not needed)

--- Rehike/AbstractModule.php
+++ Rehike/AbstractModule.php
@@ -6,15 +6,4 @@ abstract class AbstractModule {
     public function getApplication() {
         return '\Rehike\Application';
     }
-
-    public function install() {
-        $a = new static();
-        
-        // technically fires right before installation
-        if (method_exists($a, 'onInstall')) {
-            $a->onInstall();
-        }
-        
-        return $a;
-    }
 }
