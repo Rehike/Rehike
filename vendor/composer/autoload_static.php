e56ff76277b06a6f2cb0541e4d88ac76bd77fe75
Coffee <99076687+YukisCoffee@users.noreply.github.com>
2022-06-04 15:40:58 +0900

    Clean up composer packages

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,12 +7,7 @@ namespace Composer\Autoload;
 class ComposerStaticInit14befcaebdd16793d803391aa69e5f63
 {
     public static $files = array (
-        '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
-        'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
-        'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
-        '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
         'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
-        '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
     );
@@ -32,34 +27,14 @@ class ComposerStaticInit14befcaebdd16793d803391aa69e5f63
             'Symfony\\Polyfill\\Mbstring\\' => 26,
             'Symfony\\Polyfill\\Ctype\\' => 23,
             'Symfony\\Component\\CssSelector\\' => 30,
-        ),
-        'P' => 
-        array (
-            'Psr\\Http\\Message\\' => 17,
-            'Psr\\Http\\Client\\' => 16,
-            'PHPHtmlParser\\' => 14,
-        ),
-        'M' => 
-        array (
-            'MyCLabs\\Enum\\' => 13,
-        ),
-        'H' => 
-        array (
-            'Http\\Promise\\' => 13,
-            'Http\\Client\\' => 12,
-        ),
-        'G' => 
-        array (
-            'GuzzleHttp\\Psr7\\' => 16,
-            'GuzzleHttp\\Promise\\' => 19,
-            'GuzzleHttp\\' => 11,
+            'SpfPhp\\' => 7,
         ),
     );
 
     public static $prefixDirsPsr4 = array (
         'voku\\helper\\' => 
         array (
-            0 => __DIR__ . '/..' . '/voku/simple_html_dom/src/voku/helper',
+            0 => __DIR__ . '/..' . '/yukiscoffee/simple_html_dom/src/voku/helper',
         ),
         'Twig\\' => 
         array (
@@ -81,51 +56,9 @@ class ComposerStaticInit14befcaebdd16793d803391aa69e5f63
         array (
             0 => __DIR__ . '/..' . '/symfony/css-selector',
         ),
-        'Psr\\Http\\Message\\' => 
+        'SpfPhp\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-        ),
-        'Psr\\Http\\Client\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/psr/http-client/src',
-        ),
-        'PHPHtmlParser\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/paquettg/php-html-parser/src/PHPHtmlParser',
-        ),
-        'MyCLabs\\Enum\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/myclabs/php-enum/src',
-        ),
-        'Http\\Promise\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/php-http/promise/src',
-        ),
-        'Http\\Client\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/php-http/httplug/src',
-        ),
-        'GuzzleHttp\\Psr7\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/guzzlehttp/psr7/src',
-        ),
-        'GuzzleHttp\\Promise\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/guzzlehttp/promises/src',
-        ),
-        'GuzzleHttp\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/guzzlehttp/guzzle/src',
-        ),
-    );
-
-    public static $prefixesPsr0 = array (
-        's' => 
-        array (
-            'stringEncode' => 
-            array (
-                0 => __DIR__ . '/..' . '/paquettg/string-encode/src',
-            ),
+            0 => __DIR__ . '/..' . '/rehike/spfphp/src',
         ),
     );
 
@@ -142,7 +75,6 @@ class ComposerStaticInit14befcaebdd16793d803391aa69e5f63
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit14befcaebdd16793d803391aa69e5f63::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit14befcaebdd16793d803391aa69e5f63::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInit14befcaebdd16793d803391aa69e5f63::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit14befcaebdd16793d803391aa69e5f63::$classMap;
 
         }, null, ClassLoader::class);
