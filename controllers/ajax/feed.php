5be66ce1c8911500c6188322442c89a493a0d218
aubymori <aubyomori@gmail.com>
2022-07-31 13:16:00 -0500

    Fixes #41; Notification continuations, link on comment notifications

--- controllers/ajax/feed.php
+++ controllers/ajax/feed.php
@@ -7,7 +7,7 @@ return new class extends AjaxController {
 
     public function onGet(&$yt, $request) {
         if (!@$yt->signin["isSignedIn"]) { // feed_ajax is ONLY used signed in
-            echo json_encode((object)["errors"=>[]]);
+            echo json_encode((object)["errors"=>["You must be signed in"]]);
             die();
         }
 
@@ -23,6 +23,25 @@ return new class extends AjaxController {
 
             $yt->unseenCount = $updateAction->unseenCount ?? $ytdata->unseenCount ?? null;
             $yt->pollingTimeout = $updateAction->timeoutMs ?? 1800000;
+        } elseif (@$action == "continuation") {
+            $this -> template = "ajax/feed/continuation";
+            
+            if (!@$request -> params -> continuation) {
+                echo json_encode((object) [
+                    "errors" => [
+                        "Specify a continuation"
+                    ]
+                ]);
+                die();
+            }
+
+            $response = Request::innertubeRequest("notification/get_notification_menu", (object) [
+                "ctoken" => $request -> params -> continuation ?? null
+            ]);
+            $ytdata = json_decode($response);
+
+            $yt -> notifList = $ytdata -> actions[0] -> appendContinuationItemsAction -> continuationItems ?? null;
+            $yt -> nextContinuation = end($yt -> notifList) -> continuationItemRenderer -> continuationEndpoint -> getNotificationMenuEndpoint -> ctoken ?? null;
         } else {
             $this -> spfIdListteners = [
                 "yt-masthead-notifications-content"
