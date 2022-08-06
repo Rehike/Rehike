448926b478f277aa1d5fc3f28c00309a80989793
aubymori <aubyomori@gmail.com>
2022-08-05 20:09:58 -0500

    improved static router

--- controllers/rehike/static_router.php
+++ controllers/rehike/static_router.php
@@ -3,23 +3,21 @@ namespace Rehike\Controller\rehike;
 
 class StaticRouter {
     public function get(&$yt, &$template, $request) {
-        switch ($request->path[2])
-        {
-            case "logo.png":
-                header("Content-Type: image/png");
-                echo file_get_contents("static/version/logo.png");
-                exit();
-                break;
-            case "logo_small_grey.png":
-                header("Content-Type: image/png");
-                echo file_get_contents("static/version/logo_small_grey.png");
-                exit();
-                break;
-            case "branch_icon.png":
-                header("Content-Type: image/png");
-                echo file_get_contents("static/version/branch_icon.png");
-                exit();
-                break;
+        $filename = "static/";
+        for ($i = 2; $i < count($request -> path); $i++) {
+            if ($i == count($request -> path) - 1) {
+                $filename .= $request -> path[$i];
+            } else {
+                $filename .= $request -> path[$i] . "/";
+            }
+        }
+
+        if (file_exists($filename)) {
+            header("Content-Type: " . mime_content_type($filename));
+            echo file_get_contents($filename);
+            exit();
+        } else {
+            http_response_code(404);
         }
     }
 
