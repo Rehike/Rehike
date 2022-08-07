e797af1ffd1594dbd9ec279953fa8f771006c30c
aubymori <aubyomori@gmail.com>
2022-08-07 12:52:28 -0500

    improved error log; remove references to goodtube in ids

--- fatalHandler.php
+++ fatalHandler.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This should be completely redone
- * 
+ *
  * love taniko
  */
 
@@ -14,7 +14,7 @@ function fatalHandler() {
    $e = error_get_last();
    if ($e !== null && ($e['type'] == E_ERROR || $e['type'] == E_CORE_ERROR)) {
       ob_end_clean(); ob_start(); // wipe output buffer and restart
-      
+
       try {
          $buildFatalPage($e);
       } catch (Throwable $err) {
@@ -26,7 +26,7 @@ function fatalHandler() {
             echo $e["message"] . " in " . $e["file"] . ":" . $e["line"] . " <b><". $e["type"] ."></b>";
          }
       }
-      
+
       ob_end_flush();
    }
 }
@@ -38,11 +38,11 @@ $buildFatalPage = function ($e, $simple = false) use ($yt) {
    $errInfo->line = $e['line'] ?? 0;
    $errInfo->message = $e['message'] ?? '(no message)';
    $errInfo->messagePreview = fatalPreviewify($errInfo->message);
-   
+
    if (!$simple) {
       //$twig->addGlobal('errInfo', $errInfo);
       $yt->errInfo = $errInfo;
-      echo Rehike\TemplateManager::render([], 'fatal');
+      echo Rehike\TemplateManager::render([], 'rehike/fatal');
    } else {
       header('Content-Type: text/html');
       echo "<h1>Rehike pre-init error occurred</h1>";
@@ -74,12 +74,12 @@ function simplifyTwigError($msg) {
    $response = 'Fatal error (in Twig): ';
    $re = '/(Uncaught Twig\\\\Error\\\\(.*?): )|( \()/';
    preg_match_all($re, $msg, $matches, PREG_OFFSET_CAPTURE);
-   
+
    $newmsg = substr(
       $msg,
       strlen($matches[0][0][0]),
       $matches[0][1][1] - strlen($matches[0][0][0])
    );
-   
+
    return $response . ' ' . $newmsg;
 }
