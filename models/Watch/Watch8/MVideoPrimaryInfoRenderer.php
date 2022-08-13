1242c47d62b9b988bbec5af9c6dccb7ebcbe8802
Coffee <99076687+YukisCoffee@users.noreply.github.com>
2022-08-13 20:43:47 +0900

    Fix missing watch action buttons

--- models/Watch/Watch8/MVideoPrimaryInfoRenderer.php
+++ models/Watch/Watch8/MVideoPrimaryInfoRenderer.php
@@ -63,6 +63,51 @@ class MVideoPrimaryInfoRenderer
             // Create action butttons
             $orderedButtonQueue = [];
 
+            /*
+             * Updated to just hardcode this (it's generally fine, any video can be added to a playlist)
+             * 
+             * To elaborate, the previous implementation iterated the available action buttons from
+             * the InnerTube response, which usually worked.
+             * 
+             * However, as of August 2022, YouTube has been experimenting with a rewrite of the action
+             * buttons architecture, seemingly in an attempt to fix the Kevlar frontend watch page
+             * imploding cuz they have 12 action buttons with full labels on the screen and typically
+             * only report and sometimes transcript in the overflow menu.
+             * 
+             * No, of course they didn't bother fixing this in the most intuitive ways, like having
+             * hidden or dynamic label visibility (even though they always tell you what they are
+             * if you hover over the buttons anyways so THERE IS NO FUCKING POINT TO THE ADDITIONAL
+             * LABEL), instead they added a new type of button called "menuFlexibleItemRenderer" inside
+             * a new container of "flexibleItems".
+             * 
+             * It seems that, instead of the most obvious change of going from:
+             * 
+             * 👍 12K 👎 DISLIKE ➡️ SHARE ⬇️ DOWNLOAD ❤️ THANKS ✂️ CLIP ➕ SAVE  ...
+             * 
+             * to:
+             * 
+             * 👍 12K 👎  ➡️  ⬇️  ❤️  ✂️  ➕   ...
+             * 
+             * or (they will never actually use the overflow menu tho):
+             * 
+             * 👍 12K 👎  ➡️  ➕   ...
+             *          ┌───────────────┐
+             *          │ 🚩 Report     │
+             *          │ 🗒️ Transcript │
+             *          │ ⬇️ Download   │
+             *          │ ❤️ Thanks     │
+             *          │ ✂️ Clip       │
+             *          └───────────────┘
+             * 
+             * They instead decided to rework the API to "lazily" fix their broken ass frontend,
+             * and this is why sometimes "add to" would randomly not render, because this experiment
+             * only applies to ~1/10th of the unique visitor IDs.
+             */
+            if (!$dataHost::$isKidsVideo)
+            {
+                $orderedButtonQueue[] = MActionButton::buildW8AddtoButton($videoId);
+            }
+
             // Share button should always be built unless this is a
             // Kids video
             if (!$dataHost::$isKidsVideo)
@@ -70,20 +115,11 @@ class MVideoPrimaryInfoRenderer
                 $orderedButtonQueue[] = MActionButton::buildShareButton();
             }
 
-            // wtf
-            if (!$dataHost::$isKidsVideo)
-            foreach (@$info->videoActions->menuRenderer->topLevelButtons as $b)
-            if (isset($b->buttonRenderer) && ($button = @$b->buttonRenderer))
-            switch ($button->icon->iconType)
+            // Report button shows as an action button for livestreams, rather than
+            // residing in the overflow menu.
+            if ($dataHost::$isLive)
             {
-                case "PLAYLIST_ADD":
-                    // Push to the beginning of the array
-                    // since this should always come first
-                    array_unshift($orderedButtonQueue, MActionButton::buildW8AddtoButton($videoId));
-                    break;
-                case "FLAG":
-                    $orderedButtonQueue[] = MActionButton::buildReportButton();
-                    break;
+                $orderedButtonQueue[] = MActionButton::buildReportButton();
             }
 
             $this->actionButtons = &$orderedButtonQueue;
