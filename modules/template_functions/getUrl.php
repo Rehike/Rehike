204565dc6209f48f25cd798d0b01ddcfe0ea84a2
aubymori <aubyomori@gmail.com>
2022-09-05 11:19:46 -0500

    update getUrl

--- modules/template_functions/getUrl.php
+++ modules/template_functions/getUrl.php
@@ -3,6 +3,8 @@
 \Rehike\TemplateFunctions::register('getUrl', function($obj) {
     if (isset($obj->navigationEndpoint->commandMetadata->webCommandMetadata->url)) {
         return $obj->navigationEndpoint->commandMetadata->webCommandMetadata->url;
+    } elseif (isset($obj->navigationEndpoint->confirmDialogEndpoint->content->confirmDialogRenderer->confirmButton->buttonRenderer->command->urlEndpoint->url)) {
+        return $obj->navigationEndpoint->confirmDialogEndpoint->content->confirmDialogRenderer->confirmButton->buttonRenderer->command->urlEndpoint->url;
     } else {
         return '';
     }
