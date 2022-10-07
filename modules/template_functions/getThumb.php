51ccdffe413c542ead9e81f58a23e341a0596824
aubymori <aubyomori@gmail.com>
2022-10-06 20:35:16 -0500

    fix shit

--- modules/template_functions/getThumb.php
+++ modules/template_functions/getThumb.php
@@ -1,24 +1,33 @@
 <?php
 
-\Rehike\TemplateFunctions::register('getThumb', function($obj, $height = 110, $thumbList = "thumbnail") {
-    if (isset($obj -> $thumbList -> thumbnails)) {
-        $thumbs = $obj -> $thumbList -> thumbnails;
+\Rehike\TemplateFunctions::register('getThumb', function($obj, $height = 0) {
+    if (isset($obj -> thumbnail -> thumbnails)) {
+        $thumbs = $obj -> thumbnail -> thumbnails;
     } else if (isset($obj -> thumbnails)) {
         $thumbs = $obj -> thumbnails;
     }
 
-    if (!isset($thumbs)) return "//i.ytimg.com/";
+    if (!isset($thumbs)) return "//i.ytimg.com/invalid_thumb";
 
-    for ($i = 0; $i < count($thumbs); $i++) {
-        if ($thumbs[$i] -> height >= $height) {
-            $thumb =  $thumbs[$i];
+    if (isset($height) && $height != 0){
+        for ($i = 0; $i < count($thumbs); $i++) {
+            if ($thumbs[$i] -> height >= $height) {
+                $thumb = $thumbs[$i];
+            }
         }
+    } else {
+        $thumb = $thumbs[array_key_last($thumbs)];
     }
 
-    $response = (object) [];
-    $response -> url = $thumb -> url;
-    $response -> width = ceil(($thumb -> width / $thumb -> height) * $height);
-    $response -> height = $height;
+    if (isset($thumb -> width) && isset($thumb -> height)) {
+        $ratio = $thumb -> width / $thumb -> height;
 
-    return $response;
+        if ($ratio >= 1.7 && $ratio < 1.8) {
+            return $thumb -> url;
+        } else {
+            return preg_replace("/\?sqp=.*/", "", $thumb -> url);
+        }
+    } else {
+        return $thumb -> url;
+    }
 });
