626a7c3d8c404cb3df2336e30420b1f0c84b934e
aubymori <aubyomori@gmail.com>
2022-11-25 21:12:28 -0600

    agh fuck

--- modules/Rehike/Debugger/Debugger.php
+++ modules/Rehike/Debugger/Debugger.php
@@ -284,7 +284,7 @@ class Debugger
      */
     protected static function refreshInternalCondensedStatus()
     {
-        self::$condensed = RehikeConfigManager::getConfigProp("advanced.enableRehikeDebugger")
+        self::$condensed = RehikeConfigManager::getConfigProp("advanced.enableDebugger")
         ? false
         : true
         ;
