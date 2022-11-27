6c23834caad5afa9a10905d79cf4b4d582e156ad
aubymori <aubyomori@gmail.com>
2022-11-27 12:30:22 -0600

    fix 2x css???

--- modules/template_functions/cssPath.php
+++ modules/template_functions/cssPath.php
@@ -1,9 +1,11 @@
 <?php
 \Rehike\TemplateFunctions::register("cssPath", function($name, $pref, $constants) {
+    $css2x = false;
     if (isset($pref -> f4) && substr($pref -> f4, 0, 1) == "4") 
-        $prefix = "css2x";
-    else
-        $prefix = "css";
+        $css2x = true;
 
-    return $constants -> {$prefix} -> {$name} ?? "";
+    if ($css2x && isset($constants -> css2x -> {$name}))
+        return $constants -> css2x -> {$name};
+    
+    return $constants -> css -> {$name} ?? "";
 });
