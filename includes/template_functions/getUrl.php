d1a56c65d9cac142c00e70d7d67c8e7f4f0aad14
aubymori <aubyomori@gmail.com>
2023-03-06 22:10:24 -0600

    god dammit

---
+++ includes/template_functions/getUrl.php
@@ -0,0 +1,8 @@
+<?php
+
+\Rehike\TemplateFunctions::register('getUrl', function($obj) {
+    return @$obj->navigationEndpoint->commandMetadata->webCommandMetadata->url
+        ?? @$obj->navigationEndpoint->confirmDialogEndpoint->content->confirmDialogRenderer->confirmButton->buttonRenderer->command->urlEndpoint->url
+        ?? @$obj->commandMetadata->webCommandMetadata->url
+        ?? "";
+});
