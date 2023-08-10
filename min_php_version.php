a0af81da8b90af6aafa0103fa97f3bb67afc339f
aubymori <aubyomori@gmail.com>
2023-08-10 17:01:04 -0500

    Fix version

--- min_php_version.php
+++ min_php_version.php
@@ -5,7 +5,7 @@
  * Each array entry corresponds to a more minor version.
  * 
  * For example, if the minimum version was PHP 8.2, this would be:
- * array(8, 1, 0)
+ * array(8, 2, 0)
  */
 const MIN_PHP_VERSION = [
     8,
