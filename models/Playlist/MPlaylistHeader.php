989448b1cf0232a80b9519563b9346377326f786
syndiate <syndiategaming@gmail.com>
2024-01-25 20:04:23 -0500

    Fix owner text and thumbnail of album playlists

--- models/Playlist/MPlaylistHeader.php
+++ models/Playlist/MPlaylistHeader.php
@@ -18,6 +18,9 @@ class MPlaylistHeader
 
     public function __construct(object $data)
     {
+        $playlistId = $data->playlistId;
+        $isAlbum = str_starts_with($playlistId, "OL");
+
         // Title
         $this->title = ParsingUtils::getText($data->title);
         
@@ -25,25 +28,46 @@ class MPlaylistHeader
         $thumbnail = $data->playlistHeaderBanner->heroPlaylistThumbnailRenderer;
         if (isset($thumbnail->thumbnail))
         {
+            // If the playlist is an album, then the sqp parameter is needed.
+            // This results in the album thumbnail looking weird unfortunately.
             $this->thumbnail = new MPlaylistHeaderThumbnail(
-                ParsingUtils::getThumb($thumbnail->thumbnail, 126, true),
+                ParsingUtils::getThumb($thumbnail->thumbnail, 126, !$isAlbum),
                 ParsingUtils::getUrl($thumbnail->onTap),
                 ParsingUtils::getText($thumbnail->thumbnailOverlays->thumbnailOverlayHoverTextRenderer->text),
-                ParsingUtils::getUrl($data->ownerEndpoint) . "/playlists"
+                isset($data->ownerEndpoint) ? (ParsingUtils::getUrl($data->ownerEndpoint) . "/playlists") : ""
             );
         }
 
+
         // Details (author, views, etc.)
-        $this->details[] = (object) [
-            "simpleText" => ParsingUtils::getText($data->ownerText),
-            "navigationEndpoint" => $data->ownerEndpoint
+        $ownerText = ParsingUtils::getText(($data->ownerText ?? $data->subtitle));
+        if ($isAlbum)
+        {
+            // If it's an album the author text will have " • Album" appended at the end of it,
+            // so we delete that part. (This applies to all languages)
+            // TODO: More reliable method to extract the "album" part out (perhaps using i18n)
+            $ownerText = trim(explode("•", $ownerText)[0]);
+        }
+
+        $authorDetails = (object) [
+            "simpleText" => $ownerText
         ];
+        // The InnerTube response no longer includes the data for the URL 
+        // of the artist of the album (if the playlist is one), and just renders it as text.
+        if (isset($data->ownerEndpoint))
+            $authorDetails->navigationEndpoint = $data->ownerEndpoint;
+        
+        $this->details[] = $authorDetails;
+
+
         foreach ($data->byline as $byline)
         {
             $this->details[] = (object) [
                 "simpleText" => ParsingUtils::getText($byline->playlistBylineRenderer->text)
             ];
         }
+
+
 
         // Actions (play all, share, save)
         if (isset($data->playButton->buttonRenderer))
