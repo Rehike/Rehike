f75664abdf066cb319278e5b0a34da7c418283ce
Zehnter <129220104+Zehnter@users.noreply.github.com>
2024-03-12 17:51:06 +0000

    new file:   i18n/nl-NL/_rhcfg.json 	new file:   i18n/nl-NL/addto.i18n 	new file:   i18n/nl-NL/appbar.i18n 	new file:   i18n/nl-NL/attribution.i18n 	new file:   i18n/nl-NL/browse.i18n 	new file:   i18n/nl-NL/channel_switcher.i18n 	new file:   i18n/nl-NL/channels.i18n 	new file:   i18n/nl-NL/comments.i18n 	new file:   i18n/nl-NL/footer.i18n 	new file:   i18n/nl-NL/global.i18n 	new file:   i18n/nl-NL/guide.i18n 	new file:   i18n/nl-NL/masthead.i18n 	new file:   i18n/nl-NL/misc.i18n 	new file:   i18n/nl-NL/picker.i18n 	new file:   i18n/nl-NL/player_classic.i18n 	new file:   i18n/nl-NL/playlist.i18n 	new file:   i18n/nl-NL/regex.i18n 	new file:   i18n/nl-NL/results.i18n 	new file:   i18n/nl-NL/share.i18n 	new file:   i18n/nl-NL/watch.i18n

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,59 +1,60 @@
-<?php return array(
-    'root' => array(
-        'name' => '__root__',
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'reference' => '16addc0cdff806d25c13860d5ad51d403e18ec3c',
-        'type' => 'library',
-        'install_path' => __DIR__ . '/../../',
-        'aliases' => array(),
-        'dev' => true,
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
     ),
-    'versions' => array(
-        '__root__' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => '16addc0cdff806d25c13860d5ad51d403e18ec3c',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../../',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
-        'google/protobuf' => array(
-            'pretty_version' => 'v3.21.11',
-            'version' => '3.21.11.0',
-            'reference' => '8f8dc48540aed2c96eb3febcc4816f1321f66b85',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../google/protobuf',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
-        'symfony/polyfill-ctype' => array(
-            'pretty_version' => 'v1.26.0',
-            'version' => '1.26.0.0',
-            'reference' => '6fd1b9a79f6e3cf65f9e679b23af304cd9e010d4',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../symfony/polyfill-ctype',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
-        'symfony/polyfill-mbstring' => array(
-            'pretty_version' => 'v1.26.0',
-            'version' => '1.26.0.0',
-            'reference' => '9344f9cb97f3b19424af1a21a3b0e75b0a7d8d7e',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../symfony/polyfill-mbstring',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
-        'twig/twig' => array(
-            'pretty_version' => 'v3.4.3',
-            'version' => '3.4.3.0',
-            'reference' => 'c38fd6b0b7f370c198db91ffd02e23b517426b58',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../twig/twig',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
+    'reference' => 'f40395f6a1982a4d50f5416fb53b829fe061021f',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f40395f6a1982a4d50f5416fb53b829fe061021f',
     ),
+    'google/protobuf' => 
+    array (
+      'pretty_version' => 'v3.21.11',
+      'version' => '3.21.11.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8f8dc48540aed2c96eb3febcc4816f1321f66b85',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.26.0',
+      'version' => '1.26.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6fd1b9a79f6e3cf65f9e679b23af304cd9e010d4',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.26.0',
+      'version' => '1.26.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9344f9cb97f3b19424af1a21a3b0e75b0a7d8d7e',
+    ),
+    'twig/twig' => 
+    array (
+      'pretty_version' => 'v3.4.3',
+      'version' => '3.4.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c38fd6b0b7f370c198db91ffd02e23b517426b58',
+    ),
+  ),
 );
