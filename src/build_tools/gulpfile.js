ed9c7b6cb80cd7424a5c058613ef503b066bf91b
Isabella <kawapure@gmail.com>
2024-04-20 19:59:17 -0700

    [RHBuild] Introduce build system
    This commit introduces a new build system called RehikeBuild. RHBuild is
    also used as a shorthand for this new system. RehikeBuild manages building
    external source code. This mostly includes external web resources like CSS
    and JS, but it can also include generated code and other things too.
    
    RehikeBuild relies on Node.js and wraps the Gulp build system. Detailed
    documentation on how it works exactly will be written at a later date.
    
    At present, the build system only supports compiling SCSS to CSS via the
    sass compiler. A test project was included for testing purposes, filling
    something into the empty folder at src/css/. Additional compilers (for JS
    and Protobuf) will be added later.
    
    My original plan for the system was to write something completely custom in
    PHP, but it turns out that package management is actually really difficult
    and we would be better off relying on a mature system with a lot of
    preexisting packages like NPM. Gulp is a fairly nice build system that I
    and others on the Rehike development team are already familiar with, but
    the RehikeBuild code proper extends it to meet the needs we have for a
    Rehike build system.
    
    There are a couple miscellaneous changes that were made in this commit that
    were made in attempts to debug during development. I think this includes
    changing the sass version in package.json, but I don't remember, so I'm
    keeping the change in case it actually was important.
    
    Signed-off-by: Isabella <kawapure@gmail.com>

---
+++ src/build_tools/gulpfile.js
@@ -0,0 +1,78 @@
+/**
+ * @fileoverview Rehike Build System
+ * 
+ * @author Isabella <kawapure@gmail.com>
+ * @author The Rehike Maintainers
+ */
+
+const gulp = require("gulp");
+const RehikeBuild = require("./scripts/rehikebuild_main");
+
+// CSS compiler includes
+const sassBackend = require("sass");
+const gulpSassBackend = require("gulp-sass");
+const GulpSass = gulpSassBackend(sassBackend);
+
+/**
+ * Common build configuration options.
+ */
+const commonBuildCfg = {
+    base: RehikeBuild.BASE_SRC_DIR,
+    root: RehikeBuild.BASE_SRC_DIR,
+    cwd: RehikeBuild.BASE_SRC_DIR,
+};
+
+/**
+ * Wraps a Node.js stream for consumption alongside promises.
+ * 
+ * @param {Stream} stream
+ * @returns {Promise<void>}
+ */
+function promiseWrapStream(stream)
+{
+    return new Promise((resolve, reject) => {
+        stream.on("finish", resolve);
+        stream.on("end", resolve);
+        stream.on("error", reject);
+    });
+}
+
+/**
+ * All common startup tasks for the build environment.
+ * 
+ * @return {Promise<void>}
+ */
+function CommonStartupTask()
+{
+    const stream = promiseWrapStream;
+    
+    return Promise.all([
+        stream(RehikeBuild.Parser.GulpSetupRhBuildTask())
+    ]);
+}
+
+/**
+ * Runs CSS build tasks.
+ */
+function buildCss()
+{
+    return gulp.src(RehikeBuild.getBuildFilesForLanguage("css"), commonBuildCfg)
+        // Initialize the build task so that RehikeBuild actions can be applied to it:
+        .pipe(RehikeBuild.GulpInitRehikeBuildTask())
+        // Send the file contents off to the SASS compiler:
+        .pipe(GulpSass.sync({ outputStyle: "compressed" }).on("error", GulpSass.logError))
+        // Finalize the build task:
+        .pipe(RehikeBuild.GulpFinalizePathsTask())
+        .pipe(gulp.dest(RehikeBuild.BASE_SRC_DIR));
+}
+
+// Main build action
+exports.build = gulp.series(
+    CommonStartupTask,
+    gulp.parallel(
+        buildCss
+    )
+);
+
+// Build everything if no arguments are provided:
+exports.default = exports.build;
