0cb68b5c5dbe8dea31bae8fa2fec1a33e9eda4bf
Isabella <kawapure@gmail.com>
2024-05-05 16:31:38 -0700

    Clean up appbar models
    Signed-off-by: Isabella <kawapure@gmail.com>

--- models/Appbar/MAppbarNav.php
+++ models/Appbar/MAppbarNav.php
@@ -1,17 +1,38 @@
 <?php
 namespace Rehike\Model\Appbar;
 
+/**
+ * Model for an appbar navigation section.
+ * 
+ * @author Taniko Yamamoto <kirasicecreamm@gmail.com>
+ * @author The Rehike Maintainers
+ */
 class MAppbarNav
 {
-    public $items;
-    public $owner = null;
+    /**
+     * All navigation links in the appbar navigation section.
+     * 
+     * @var MAppbarNavItem[]
+     */
+    public array $items;
+    
+    /**
+     * The owner channel, or null. 
+     */
+    public ?MAppbarNavOwner $owner = null;
 
-    public function addItem($title, $href, $status)
+    /**
+     * Add a navigation link item to this appbar navigation section.
+     */
+    public function addItem(string $title, string $href, int $status): void
     {
         $this->items[] = new MAppbarNavItem($title, $href, $status);
     }
 
-    public function addOwner($title, $href, $thumbnail)
+    /**
+     * Add an owner channel link to this appbar navigation section.
+     */
+    public function addOwner(string $title, string $href, string $thumbnail): void
     {
         $this->owner = new MAppbarNavOwner($title, $href, $thumbnail);
     }
