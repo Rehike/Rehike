94208a55582e00eb8242d240accb6c2aec8d6b7a
Isabella <kawapure@gmail.com>
2024-07-24 18:59:02 -0700

    Support retrieving language from cookies
    We never got around to this, but it's an important to change for when we
    finally introduce pickers.
    
    Signed-off-by: Isabella <kawapure@gmail.com>

--- modules/Rehike/i18n/BootServices.php
+++ modules/Rehike/i18n/BootServices.php
@@ -4,6 +4,7 @@ namespace Rehike\i18n;
 use Rehike\ConfigManager\Config;
 use Rehike\i18n\Internal\Core as I18nCore;
 use Rehike\YtApp;
+use Rehike\Validation\ValidHostLanguages;
 
 /**
  * Code organization stub for i18n boot services.
@@ -16,13 +17,24 @@ class BootServices
     /**
      * Perform setup duties.
      */
-    public static function boot()
+    public static function boot(): void
     {
         \Rehike\Profiler::start("i18nboot");
         $langId = Config::getConfigProp("hidden.language");
 
         i18n::getConfigApi()
             ->setRootDirectory($_SERVER["DOCUMENT_ROOT"] . "/i18n");
+            
+        if (isset($_COOKIE["hl"]))
+        {
+            $validator = new ValidHostLanguages();
+            $targetHl = I18nCore::validateHlGl($_COOKIE["hl"]);
+            
+            if ($validator->validateString($targetHl))
+            {
+                $langId = $targetHl;
+            }
+        }
 
         if ($langId == null || !i18n::isValidLanguageId($langId))
         {
