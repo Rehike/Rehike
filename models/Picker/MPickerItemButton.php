8f3690339cd83de392a7fe8fb73d82efae3eb36d
Isabella <kawapure@gmail.com>
2024-07-28 21:08:06 -0700

    Picker: Active country name & language tooltips.
    The active country name is now displayed on the country picker button, and
    the name of a different language is shown in the current language as a
    hover tooltip.
    
    Signed-off-by: Isabella <kawapure@gmail.com>

--- models/Picker/MPickerItemButton.php
+++ models/Picker/MPickerItemButton.php
@@ -12,15 +12,22 @@ use Rehike\Model\Common\MButton;
 class MPickerItemButton
 {
     public string|object $title;
+    public string|object $tooltip;
     public string $name;
     public string $value;
     public bool $isCurrent = false;
     
-    public function __construct(string $title = "", string $name = "", string $value = "", bool $isCurrent = false)
+    public function __construct(string|object $title = "", string $name = "", string $value = "", bool $isCurrent = false)
     {
         $this->title = $title;
+        $this->tooltip = $title;
         $this->name = $name;
         $this->value = $value;
         $this->isCurrent = $isCurrent;
+    }
+    
+    public function setTooltip(string|object $tooltip): void
+    {
+        $this->tooltip = $tooltip;
     }
 }
